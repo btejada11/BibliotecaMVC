534fe64dd4ff2227ae5f6a3f817a3ce43ece0278
btejada11 <leoneltejada5@gmail.com>
2026-08-15 09:06:27 -0600

    Cambios previos

---
+++ Models/ServiceClass.cs
@@ -0,0 +1,11 @@
+﻿namespace EjercicioPractica_S1.Models
+{
+    public class ServiceClass
+    {
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Controller { get; set; } = string.Empty;
+        public string Action { get; set; } = string.Empty;
+    }
+}
