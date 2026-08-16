f0fbc1d400a87729e1642323f1b1118db5101540
btejada11 <leoneltejada5@gmail.com>
2026-08-15 18:45:42 -0600

    Semana 5

---
+++ Models/ComponentViewModels.cs
@@ -0,0 +1,17 @@
+﻿namespace EjercicioPractica_S1.Models
+{
+    public class FeatureViewModel
+    {
+        public string IconClass { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string BorderColor { get; set; } // Opcional (ej: #ffc107 o "gold")
+    }
+
+    public class BannerViewModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string BackgroundImage { get; set; } // Opcional
+    }
+}
